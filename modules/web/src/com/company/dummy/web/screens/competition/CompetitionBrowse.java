42049e1e94143b8cf4da707a167dfdd126a39261
David Tokarenko <d.tokarenko@haulmont.com>
2020-06-02 17:43:45 +0400

    task №5 - Действие «копировать».

--- modules/web/src/com/company/dummy/web/screens/competition/CompetitionBrowse.java
+++ modules/web/src/com/company/dummy/web/screens/competition/CompetitionBrowse.java
@@ -1,11 +1,46 @@
 package com.company.dummy.web.screens.competition;
 
+
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.chile.core.model.MetaProperty;
+import com.haulmont.chile.core.model.Session;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.dummy.entity.Competition;
+
+import javax.inject.Inject;
+import java.util.Collection;
 
 @UiController("dummy$Competition.browse")
 @UiDescriptor("competition-browse.xml")
 @LookupComponent("competitionsTable")
 @LoadDataBeforeShow
 public class CompetitionBrowse extends StandardLookup<Competition> {
+
+    @Inject
+    private Metadata metadata;
+    @Inject
+    private GroupTable<Competition> competitionsTable;
+    @Inject
+    private CollectionContainer<Competition> competitionsDc;
+
+    @Subscribe("competitionsTable.copy")
+    public void onCompetitionsTableCopy(Action.ActionPerformedEvent event) {
+        if (competitionsTable.getSingleSelected() != null) {
+            Session session = metadata.getSession();
+            MetaClass metaClasses = session.getClassNN(Competition.class);
+            Collection<MetaProperty> metaProperties = metaClasses.getOwnProperties();
+            Competition copyCompetition = (Competition) metadata.create(metaClasses);
+            for (MetaProperty property : metaProperties) {
+                if (!property.getJavaType().isInterface())
+                    copyCompetition.setValue(property.getName(),
+                            competitionsTable.getSingleSelected().getValue(property.getName()),
+                            false);
+            }
+            competitionsDc.getMutableItems().add(copyCompetition);
+        }
+    }
 }
